510790cf6d0834b7ea1bd32a55073ee272f74277
juttameerhamza <juttameerhamza@gmail.com>
2024-08-04 22:44:10 +0500

    feat(linting): added linting with eslint and code formating with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import * as React from "react";
-
 function App() {
   return (
     <main>
-      <p className="text-lg font-medium">CRA App</p>
+      <p className='text-lg font-medium'>CRA App</p>
     </main>
   );
 }
